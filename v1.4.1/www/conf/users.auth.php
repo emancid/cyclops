033df0094127e9157b80a60ed5a771b82655d2e2
Ignacio Garcia <ignaciogarciahoyos@gmail.com>
2017-06-09 13:17:18 +0200

    web pages fixes

---
+++ v1.4.1/www/conf/users.auth.php
@@ -0,0 +1,11 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+admin:$1$qdX78kfK$0Wyjqz/KFUn1dkA/2fh4H0:admin bullx:no@mail.com:admin,user
