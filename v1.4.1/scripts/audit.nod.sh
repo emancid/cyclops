81715943880bc0fc32d11eebd98617eee7a8f430
Ignacio Garcia <ignaciogarciahoyos@gmail.com>
2017-08-29 09:20:18 +0200

    audit script fix small bug

--- v1.4.1/scripts/audit.nod.sh
+++ v1.4.1/scripts/audit.nod.sh
@@ -507,7 +507,7 @@ generate_wiki_view()
 		} ' )
 	_output_activity=$( echo "${_output_activity}" | sed -e "s/OK/$_color_ok &/" -e "s/UP/$_color_up &/" -e "s/DOWN/$_color_down &/" -e "s/FAIL/$_color_fail &/" -e "s/DRAIN/$_color_disable &/" -e "s/CONTENT/$_color_unknown &/" ) 
 	_output_activity=$( echo "${_output_activity}" | sed -e "s/ALERT/$_color_fail &/g" -e "s/INFO/$_color_up &/g" -e "s/REPAIR/$_color_mark &/" -e  "s/DIAGNOSE/$_color_check &/g" -e "s/STATUS/$_color_up &/g" -e "s/SOLVED/$_color_ok &/g" )
-	_output_activity=$( echo "${_output_activity}" | sed -e "s/UNLINK/$_color_mark &/" -e "s/LINK/$_color_mark &/" -e "s/REACTIVE/$_color_rzr &/" -e "s/CONTENT/$_color_down &/" )
+	_output_activity=$( echo "${_output_activity}" | sed -e "s/UNLINK/$_color_mark KICKOUT/" -e "s/LINK/$_color_mark &/" -e "s/REACTIVE/$_color_rzr &/" -e "s/CONTENT/$_color_down &/" )
 	_output_activity=$( echo "${_output_activity}" | sed -e 's/^/|  /' -e 's/$/  |/' -e 's/;/  |  /g' )
 
 	#- BITACORA INFO
@@ -1095,7 +1095,7 @@ show_data()
 
 				_output_gen_bitacora=$( echo "${_output_gen_bitacora}" | sed -e "s/OK/$_color_ok &/" -e "s/UP/$_color_up &/" -e "s/DOWN/$_color_down &/" -e "s/FAIL/$_color_fail &/" -e "s/DRAIN/$_color_disable &/" -e "s/CONTENT/$_color_unknown &/" ) 
 				_output_gen_bitacora=$( echo "${_output_gen_bitacora}" | sed -e "s/ALERT/$_color_fail &/g" -e "s/INFO/$_color_up &/g" -e "s/REPAIR/$_color_mark &/" -e  "s/DIAGNOSE/$_color_check &/g" -e "s/STATUS/$_color_up &/g" -e "s/SOLVED/$_color_ok &/g" )
-				_output_gen_bitacora=$( echo "${_output_gen_bitacora}" | sed -e "s/UNLINK/$_color_mark &/" -e "s/LINK/$_color_mark &/" -e "s/REACTIVE/$_color_rzr &/" -e "s/CONTENT/$_color_down &/" -e "s/DISABLE/$_color_disable &/g" )
+				_output_gen_bitacora=$( echo "${_output_gen_bitacora}" | sed -e "s/UNLINK/$_color_mark KICKOUT/" -e "s/LINK/$_color_mark &/" -e "s/REACTIVE/$_color_rzr &/" -e "s/CONTENT/$_color_down &/" -e "s/DISABLE/$_color_disable &/g" )
 				_output_gen_bitacora=$( echo "${_output_gen_bitacora}" | sed -e "s/TESTING/$_color_check &/g" -e "s/UPGRADE/$_color_mark &/" -e "s/ENABLE/$_color_ok &/g" -e "s/INTERVENTION/$_color_mark &/g" -e "s/ISSUE/$_color_fail &/" )
 
 				echo "====== $( echo $_par_node | tr [:lower:] [:upper:] ) BITACORA ======"
